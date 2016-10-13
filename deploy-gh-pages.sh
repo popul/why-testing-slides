890de1d59f656e7290b60247fe27fe1ca0b2b78e
Paul Musso <paul.musso@orange.com>
2016-10-13 09:21:05 +0200

    chore: tweak build

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,7 +1,7 @@
 cd dist
 git init
 git config user.name "Paul Musso"
-git config user.email "paul.musso@orange.com"
+git config user.email "cpopul@gmail.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
